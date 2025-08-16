e96722ce524f6cb1bafcac49e5c048453d0878d1
Alef <aleftorresbr@gmail.com>
2025-08-16 15:52:59 -0300

    JPA respository, injecao de dependencias e database seeding

---
+++ src/main/java/com/aleftorres/curso/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.aleftorres.curso.repositories;
+
+import com.aleftorres.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
