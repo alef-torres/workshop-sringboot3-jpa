01ad215b5ef5c2fdaa657af2f796119fc2c5025a
Alef <aleftorresbr@gmail.com>
2025-08-16 16:55:05 -0300

    Camada de serviços, registro de componentes

--- src/main/java/com/aleftorres/curso/resource/UserResource.java
+++ src/main/java/com/aleftorres/curso/resource/UserResource.java
@@ -1,18 +1,32 @@
 package com.aleftorres.curso.resource;
 
 import com.aleftorres.curso.entities.User;
+import com.aleftorres.curso.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        return ResponseEntity.ok().body(new User(1L,"teste","teste","teste","teste"));
+    public ResponseEntity<List<User>> findAll() {
+        List<User> lista = service.findAll();
+        return ResponseEntity.ok().body(lista);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
