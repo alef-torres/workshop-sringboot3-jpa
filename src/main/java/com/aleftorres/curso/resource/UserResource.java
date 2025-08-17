48c6135282e0da7a4eac9260c54fc9bf37995340
Alef <aleftorresbr@gmail.com>
2025-08-17 12:46:36 -0300

    Enums de orderm dos pedidos

--- src/main/java/com/aleftorres/curso/resource/UserResource.java
+++ src/main/java/com/aleftorres/curso/resource/UserResource.java
@@ -4,10 +4,7 @@ import com.aleftorres.curso.entities.User;
 import com.aleftorres.curso.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +14,7 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
